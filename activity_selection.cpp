8b40b8f5f632a5176c5ee321424126b4702ce5ce
Shahibur Rahman <50083925+Shahibur66@users.noreply.github.com>
2020-05-09 00:31:49 +0600

    Add files via upload

--- activity_selection.cpp
+++ activity_selection.cpp
@@ -11,7 +11,6 @@ void activitySelection(Activity activities[], int n);
 
 int main(void) {
 
-  //activities
   Activity activities[8] = {
     {"a1", 1, 3},
     {"a2", 0, 4},
@@ -23,7 +22,6 @@ int main(void) {
     {"a8", 4, 5}
   };
 
-  //number of activities
   int n = 8;
 
   activitySelection(activities, n);
@@ -32,13 +30,11 @@ int main(void) {
 }
 
 void activitySelection(Activity activities[], int n) {
-  //variables
+
   int i, j;
 
   Activity temp;
 
-  //step 1
-  //sort the activities as per finishing time in ascending order
   for(i = 1; i < n; i++) {
     for(j = 0; j < n - 1; j++){
       if(activities[j].finish > activities[j+1].finish) {
@@ -49,22 +45,19 @@ void activitySelection(Activity activities[], int n) {
     }
   }
 
-  //sorted
+ 
   printf("Sorted activities as per finish time (ascending order)\n");
   printf("%10s %10s %10s\n", "Activity", "Start", "Finish");
   for(i = 0; i < n; i++) {
     printf("%10s %10i %10i\n", activities[i].id, activities[i].start, activities[i].finish);
   }
 
-  //step 2
-  //select the first activity
+
   printf("-----Selected Activities-----\n");
   printf("%10s %10s %10s\n", "Activity", "Start", "Finish");
   printf("%10s %10i %10i\n", activities[0].id, activities[0].start, activities[0].finish);
 
-  //step 3
-  //select next activity whose start time is greater than
-  //or equal to the finish time of the previously selected activity
+ 
   i = 0;
   for(j = 1; j < n; j++) {
     if(activities[j].start >= activities[i].finish) {
