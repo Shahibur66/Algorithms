ebdea28ef41b920e3d72262c0df5246b99a18389
Shahibur Rahman <50083925+Shahibur66@users.noreply.github.com>
2020-05-11 11:44:28 +0600

    Add files via upload

--- Floyed_WarshallAlgo.cpp
+++ Floyed_WarshallAlgo.cpp
@@ -23,7 +23,6 @@ typedef struct results
 
 int main(void) {
 
-  //distance array
 
   int D[MAX][MAX] = {
     {INF, 5, 1, 2},
@@ -32,7 +31,6 @@ int main(void) {
     {2, INF, 4, INF}
   };
 
-  //sequence array
 
   int S[MAX][MAX] = {
     {INF, 2, 3, 4},
@@ -41,7 +39,7 @@ int main(void) {
     {1, 2, 3, INF}
   };
 
-  int size = 4;    //total number of vertices in the graph
+  int size = 4;   
 
   floyds(D, S, size);
 
@@ -51,14 +49,8 @@ int main(void) {
 void floyds(int D[][MAX], int S[][MAX], int size) {
   int i, j, k, l;
 
-  //arrays to hold data for current iteration
-  /*
-    we have created arrays of size 10x10 (MAX x MAX)
-    but we will be using only 4x4 as the graph has 4 vertices
-  */
   int Dk[MAX][MAX], Sk[MAX][MAX];
 
-  //set Dk and Sk to 0
   for(i = 0; i < size; i++) {
     for(j = 0; j < size; j++) {
       if(i == j) {
@@ -72,48 +64,39 @@ void floyds(int D[][MAX], int S[][MAX], int size) {
   }
 
 
-  //iteration start here ** ITERATION METHOD **
 
   for(k = 0; k < size; k++) {
 
-    //step 1:
-    //for each iteration we copy the kth row and kth column to
-    //the current array
     for(l = 0; l < size; l++) {
-      //copy row
+
       Dk[k][l] = D[k][l];
       Sk[k][l] = S[k][l];
 
-      //copy column
       Dk[l][k] = D[l][k];
       Sk[l][k] = S[l][k];
     }
 
-    //step 2:
-    //compute the distance and sequence value for
-    //current iteration
     for(i = 0; i < size; i++) {
 
-      //for kth iteration we skip the kth row
+    
       if(i == k) {
         continue;
       }
 
       for(j = 0; j < size; j++) {
 
-        //for kth iteration we skip the kth column
         if(j == k) {
           continue;
         }
-        //if i and j are same i.e., referring to same vertex we skip it
+       
         if(i == j) {
           continue;
         }
 
-        //checking
+      
         if(D[i][j] > D[i][k] + D[k][j]) {
           Dk[i][j] = D[i][k] + D[k][j];
-          Sk[i][j] = (k+1);  //kth iteration, as indexing starts from 0 so, we add 1
+          Sk[i][j] = (k+1);  
         } else {
           Dk[i][j] = D[i][j];
           Sk[i][j] = S[i][j];
@@ -121,8 +104,6 @@ void floyds(int D[][MAX], int S[][MAX], int size) {
       }
     }
 
-    //step 3:
-    //copy content of Dk and Sk to D and S
     for(i = 0; i < size; i++) {
       for(j = 0; j < size; j++) {
         D[i][j] = Dk[i][j];
